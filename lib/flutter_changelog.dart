71d33f744648363544da9bbfdbb5e2cb85d191b6
Paul Halliday <paulhalliday@desktop-lt1n1qq.home>
2020-02-19 01:23:43 +0000

    WIP: ChangeLogScreen

--- lib/flutter_changelog.dart
+++ lib/flutter_changelog.dart
@@ -1,7 +1,33 @@
 library flutter_changelog;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+/// Opens up a [ChangeLogScreen]
+class ChangeLogScreen extends StatelessWidget {
+  final String title;
+  final String markdownData;
+  final ScrollController scrollController = ScrollController();
+
+  ChangeLogScreen({
+    @required this.title,
+    @required this.markdownData,
+  })  : assert(title != null),
+        assert(markdownData != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: SafeArea(
+        child: Markdown(
+          controller: scrollController,
+          data: markdownData,
+        ),
+      ),
+    );
+  }
 }
