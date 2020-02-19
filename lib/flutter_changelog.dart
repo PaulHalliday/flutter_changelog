232560431d7b72574a002ade9211c8be12d58334
Paul Halliday <paulhalliday@desktop-lt1n1qq.home>
2020-02-19 01:51:41 +0000

    Changelog page

--- lib/flutter_changelog.dart
+++ lib/flutter_changelog.dart
@@ -1,33 +1,50 @@
 library flutter_changelog;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 /// Opens up a [ChangeLogScreen]
 class ChangeLogScreen extends StatelessWidget {
-  final String title;
+  final Widget title;
   final String markdownData;
+  final String markdownFilePath;
+
   final ScrollController scrollController = ScrollController();
 
   ChangeLogScreen({
     @required this.title,
-    @required this.markdownData,
-  })  : assert(title != null),
-        assert(markdownData != null);
+    this.markdownData,
+    this.markdownFilePath,
+  }) : assert(title != null);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: title,
       ),
       body: SafeArea(
-        child: Markdown(
-          controller: scrollController,
-          data: markdownData,
-        ),
-      ),
+          child: markdownFilePath != null
+              ? FutureBuilder(
+                  future: rootBundle.loadString(markdownFilePath),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.hasData) {
+                      return Markdown(
+                        controller: scrollController,
+                        data: snapshot.data,
+                      );
+                    }
+
+                    return Center(child: CircularProgressIndicator());
+                  },
+                )
+              : Markdown(
+                  controller: scrollController,
+                  data: markdownData,
+                )),
     );
   }
 }
